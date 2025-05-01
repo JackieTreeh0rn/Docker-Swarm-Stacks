62f33e5183d60dd272a407791486abad9c145a7a
JackieTreeh0rn <1523839+JackieTreeh0rn@users.noreply.github.com>
2025-05-01 02:27:21 -0400

    build-local big-agi

--- Ai/big-AGI/build-local.sh
+++ Ai/big-AGI/build-local.sh
@@ -13,7 +13,5 @@ git clone --depth 1 --single-branch https://github.com/JackieTreeh0rn/big-AGI bu
 # Replace config files as needed
 # cp -rvf ./middleware.ts ./build/ #for basic auth
 
-# BUILD - DOCKER HUB
-docker login
-# docker buildx build --platform "linux/amd64,linux/arm64" -t jackietreehorn/${STACK_NAME}:latest --push -f ./build/Dockerfile ./build
-docker build -t jackietreehorn/${STACK_NAME}:latest --push -f ./build/Dockerfile ./build
+# BUILD - local docker
+docker build -t ${STACK_NAME} -f ./build/Dockerfile ./build
